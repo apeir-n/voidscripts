46515dbb4d90c5a0e34fc54e29a92aa365b3e66a
apeir-n <brshellito@gmail.com>
2025-09-17 22:20:42 -0400

    some prettification to updater scripts

--- status/time.sh
+++ status/time.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source $HOME/scripts/status/notify.sh
+
 clock=$(date '+%I')
 
 case $clock in
@@ -19,8 +21,9 @@ case $clock in
 esac
 
 case $BUTTON in
-    1) setsid -f st -g 100x20 -e /home/ch_rism_/.local/share/cargo/bin/tuime --format "%I:%M" -f font3d -c cyan -c green ;;
-    3) setsid -f st -e /home/ch_rism_/.cargo/bin/clock-rs -tsBc green --fmt '%y%m%d' ;;
+    1) notify 260 "$(date)" ;;
+    3) setsid -f st -g 100x20 -e /home/ch_rism_/.local/share/cargo/bin/tuime --format "%I:%M" -f font3d -c cyan -c green ;;
+    #3) setsid -f st -e /home/ch_rism_/.cargo/bin/clock-rs -tsBc green --fmt '%y%m%d' ;;
 esac
 
 date "+$icon  %I:%M%P ] "
